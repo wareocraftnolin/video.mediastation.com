5cf64d8fea0bc68d27281547093326b1d95b51c6
Wareocraft <78630991+wareocraftnolin@users.noreply.github.com>
2023-05-05 14:59:25 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,3 @@
+function onSubmit() {
+          if (document.getElementById('password').value == 'root') {window.location.href = 'home.html'; }else{ alert('Please check your passcode and try again');}
+     }
