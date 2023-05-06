9d1b99bf1b693e3f089280516a3102f23d920108
Wareocraft <78630991+wareocraftnolin@users.noreply.github.com>
2023-05-06 09:17:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 function onSubmit() {
-          if (document.getElementById('password').value == 'root') {window.location.href = 'home.html'; }else{ alert('Please check your passcode and try again');}
+          if (document.getElementById('password').value == 'root') {window.location.href = 'https://wareocraftnolin.github.io/video.mediastation.com/home.html'; }else{ alert('Please check your passcode and try again');}
 }
