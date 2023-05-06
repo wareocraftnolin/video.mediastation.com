0f4c351a37180232339ad10ab8bf406c4187bbf6
Wareocraft <78630991+wareocraftnolin@users.noreply.github.com>
2023-05-06 09:16:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 function onSubmit() {
-          if (document.getElementById('login-password').value == 'root') {window.location.href = 'home.html'; }else{ alert('Please check your passcode and try again');}
-     }
+          if (document.getElementById('password').value == 'root') {window.location.href = 'home.html'; }else{ alert('Please check your passcode and try again');}
+}
